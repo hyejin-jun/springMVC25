65cdce0946844ef89a36a5e3c0ecf5ad93b9cbf0
220-12\hyejin <h77200407@gmail.com>
2025-07-15 10:50:03 +0900

    2025.07.15 최종 브랜치 커밋

--- src/test/java/org/mbc/mapper/BoardMapperTests.java
+++ src/test/java/org/mbc/mapper/BoardMapperTests.java
@@ -1,7 +1,5 @@
 package org.mbc.mapper;
 
-import javax.swing.border.Border;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mbc.domain.BoardVO;
@@ -31,69 +29,84 @@ public class BoardMapperTests {
 		//                                log.info 콘솔에 출력(board.toString)
 	}
 	
-@Test	
-public void testGetListXML() {
+	
+	@Test // import org.junit.Test; (메서드 단위로 테스트)
+	public void testGetListXML() {
 		
 		mapper.getList2().forEach(board -> log.info(board) );
-}
-
-@Test
-public void testInsert() {
+		// 인터페이스.메서드.for문   결과객체 
+		//                             -> 람다식(인터페이스용)
+		//                                log.info 콘솔에 출력(board.toString)
+	}
 	
-	BoardVO board = new BoardVO();
-	board.setTitle("제발 오류 없어라");
-	board.setContent("이번에는 누구냐");
-	board.setWriter("김기원");
 	
-	mapper.insert(board);
 	
-	log.info(board);
+	@Test
+	public void testInsert() {
+		
+		BoardVO board = new BoardVO(); 
+		board.setTitle("제발 오류없어라");
+		board.setContent("이번엔 누구냐!!!");
+		board.setWriter("김기원");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+		
+	}
 	
-}
-
-
-		@Test
-		public void testInsertSelectKey() {
-			BoardVO board = new BoardVO();
-			board.setTitle("이번에는 제발 번호 나와라");
-			board.setContent("내 게시물 번호는?: ");
-			board.setWriter("김기원");
-			
-			mapper.insertSelectKey(board);
-			
-			log.info(board);
-			log.info("내가 만든 게시물에 번호는: " + board.getBno());
-}
-
-		@Test
-		public void testRead() {
-			BoardVO board = mapper.read(1L);  // 1 번 게시물을 리드 메서드로 보내고 객체로 받음
-			
-			log.info(board);
-			
-		}
+	
+	
+	@Test
+	public void testInsertSelectKey() {
 		
-		@Test
-		public void testDelete() {
-			
-			int count = mapper.delete(3L);
-			log.info("삭제된 개수 출력: " + count + "건");
-		}
+		BoardVO board = new BoardVO();
+		board.setTitle("이번엔 제발!!!! 번호나와롸");
+		board.setContent("내 게시물에 번호는? ");
+		board.setWriter("김기원");
 		
-		@Test
-		public void testUpdate() {
-			
-			BoardVO board = new BoardVO();
-			
-			board.setBno(5L);
-			board.setTitle("수정된 제목");
-			board.setContent("수정된 내용");
-			board.setWriter("김수정");
-			
-			int count = mapper.update(board);
-			log.info("UPDATE COUNT: " + count + "건");
-			log.info("수정된 객체 출력: " + board);
-		}
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+		log.info("내가만든 게시물의 번호는 : " + board.getBno());
+		
+	}
+	
+	@Test
+	public void testRead() {
+		
+		BoardVO board = mapper.read(1L); // 1번 게시물을 read 메서드로 보내고 객체로 받는다.
+		
+		log.info(board);
+		
+	}
+	
+	
+	@Test
+	public void testDelete() {
+		
+		int count = mapper.delete(3L);
+		log.info("삭제된 갯수 출력 : " + count + "건");
+	}
+	
+	
+	
+	@Test
+	public void testUpdate() {
+		
+		BoardVO board = new BoardVO();
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("김수정");
+		
+		int count = mapper.update(board);
+		log.info("수정된 갯수 : " + count + "건");
+		log.info("수정된 객체 출력 :" + board);
 		
 		
+		
+		
+		
+	}
 }
